703cafe1630fdefd5c4413da0daa7261859ae8e7
Ömer Faruk Gözegir <gozgirfaruk@gmail.com>
2024-04-07 15:56:54 +0300

    admin UI Added

---
+++ MiniCoreProject/Controllers/ExperienceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MiniCoreProject.Controllers
+{
+	public class ExperienceController : Controller
+	{
+		public IActionResult ExpList()
+		{
+			return View();
+		}
+	}
+}
